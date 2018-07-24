43bf240628c7c01edcb68bc78fb33a26c359d4c6
Samuel Shaqqur <samuelshaqqur@Samuels-MBP.frontierlocal.net>
2018-07-23 22:59:19 -0700

    sa

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -7,16 +7,16 @@
 	
 	$email_subject = "New Form Submission";
 	
-	$email_body = "Client Name: $name.\n".
-					"Client Email: $visitor_email.\n".
-					"Client Message: $message.\n";
+	$email_body = "Client Name: ".$name." .\n".
+					"Client Email:".$visitor_email.".\n".
+					"Client Message: ".$message.".\n";
 					
 	$to = "samuelshaq@gmail.com";
 	
 	$headers = "From: ".$email_from;
-	$headers .= "Reply-To: $visitor_email \r\n";
+	$headers .= "Reply-To: ".$visitor_email."\r\n";
 	
 	mail($to,$email_subject,$email_body,$headers);
 	
-	header("Location: index.html?mailsend")
+	header("Location: index.html")
 ?>
