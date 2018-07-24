b3304561ed2a78850f43e52f9fcddf148dbdf9e7
Samuel Shaqqur <samuelshaqqur@Samuels-MBP.frontierlocal.net>
2018-07-23 22:50:57 -0700

    s
    s

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -11,12 +11,12 @@
 					"Client Email: $visitor_email.\n".
 					"Client Message: $message.\n";
 					
-	$to = "samuelshaq@gmail.com"
+	$to = "samuelshaq@gmail.com";
 	
-	$headers = "From: $email_from \r\n";
+	$headers = "From: ".$email_from;
 	$headers .= "Reply-To: $visitor_email \r\n";
 	
 	mail($to,$email_subject,$email_body,$headers);
 	
-	header("Location: index.html")
+	header("Location: index.html?mailsend")
 ?>
